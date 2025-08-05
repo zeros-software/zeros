5626f17209ad5fac72ff786e6324d754efb0670d
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2025-08-05 13:03:48 -0300

    .

--- zeros/src/app/page.jsx
+++ zeros/src/app/page.jsx
@@ -144,7 +144,7 @@ export default function Home() {
       <div ref={servicesRef} style={{ height: "100vh", width: "100%" }} className="flex flex-col px-5">
         <Services />
       </div>
-      <div ref={contactRef} style={{ height: "100vh", width: "100%" }} className="flex flex-col">
+      <div ref={contactRef} style={{ height: "100vh", width: "100%" }} className="flex flex-col px-6">
         <Contact />
       </div>
 
--- zeros/src/components/Contact.jsx
+++ zeros/src/components/Contact.jsx
@@ -1,24 +1,24 @@
 export default function Contact() {
     return (
         <div className="flex flex-col bg-black h-screen text-white relative">
-            <div className="px-6 py-4 pt-16 flex flex-col">
+            <div className="py-4 pt-16 flex flex-col">
                 <span className="sf-pro-bold text-7xl">Tell us about you</span>
                 <span className="text-4xl mt-4 w-130">Let us know how can we help you achieve your goal.</span>
                 <div className="w-full bg-white h-2 mt-6"></div>
                 <span className="mt-6 text-2xl">Type here (feel free to drop images)</span>
-                <div className="absolute bottom-10 flex flex-row gap-10 items-center sf-pro font-medium text-2xl underline underline-offset-5">
-                    <a>
-                        → Instagram
-                    </a>
-                    <a>
-                        → Github
-                    </a>
-                    <a>
-                        → LinkedIn
-                    </a>
-                    <a>
-                        → Mail
-                    </a>
+                <div className="absolute bottom-10 flex flex-row items-center sf-pro font-medium text-2xl underline underline-offset-5 justify-between w-full">
+                    <div className="gap-10 flex items-start">
+                        <a>
+                            → Github
+                        </a>
+                        <a>
+                            → LinkedIn
+                        </a>
+                        <a>
+                            → Mail
+                        </a>
+                    </div>
+                    <div>2025</div>
                 </div>
             </div>
         </div>
