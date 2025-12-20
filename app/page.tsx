6e1458effdd60b7c90c13c7c217b080b6bec2d4e
Lucas Grasso <lucasgrassoramos@gmail.com>
2025-12-20 00:25:31 -0300

    about section

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import { ProjectShowcase } from "@/components/Project-showcase/project-showcase"
 import { CTA } from "@/components/cta/cta"
 import { Header } from "@/components/Header/header"
 import { Footer } from "@/components/Footer/footer"
+import { About } from "@/components/About/about"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
       <Header />
       <Hero />
+      <About />
       <Services />
       {/* <ProjectShowcase /> */}
       <CTA />
---
+++ components/About/about.tsx
@@ -0,0 +1,12 @@
+export function About() {
+  return (
+    <section id="about" className="">
+      <div className="container mx-auto px-6 py-24 lg:px-8 lg:py-32">
+        <h2 className="text-3xl font-bold text-white mb-8 lg:text-4xl">Who are we</h2>
+        <p className="text-xl leading-relaxed text-white/90 max-w-4xl lg:text-2xl">
+          We are a team of developers and designers, based in Buenos Aires, <span className="text-[#74ACDF]">Argentina</span>, who factors software into your businesses and ideas, transforming what's in your mind to code and design.
+        </p>
+      </div>
+    </section>
+  )
+}
